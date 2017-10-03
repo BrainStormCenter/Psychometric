84b8165b3707a21575f6e1b0a2c158680189e466
Jason <chejux@gmail.com>
2017-10-03 09:59:49 -0500

    There is a mismatch between the subject folder and MC file

--- JC_checkmotion.m
+++ JC_checkmotion.m
@@ -24,8 +24,9 @@ for fc = 1:numel(rpfiles)
 end
 rpfiles = cat(1, rpfiles{:});
 
-% iterate over all files
-rpfolders = {rpfiles.folder};
+%   iterate over all files
+%rpfolders = {rpfiles.folder};
+rpfolders = {subfolders.name};
 rpfiles = {rpfiles.name};
 rpoutliers = cell(numel(rpfiles), 1);
 for fc = 1:numel(rpfiles)
@@ -36,7 +37,7 @@ for fc = 1:numel(rpfiles)
     rpoutliers{fc} = 1 + find(any(abs(rpd(:, 1:3)) > transthresh, 2) | ...
         any(abs(rpd(:, 4:6)) > rotthresh, 2));
 
-    % print out information
+%     % print out information
     olist = sprintf('%d, ', rpoutliers{fc});
     olist(end-1:end) = [];
     fprintf('%-88s: %d ([%s])\n', rpfiles{fc}, numel(rpoutliers{fc}), olist);
