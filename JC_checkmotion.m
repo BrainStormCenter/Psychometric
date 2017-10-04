5368d836b8721352aa9427279375a30f09bcc4c0
Jason <chejux@gmail.com>
2017-10-04 13:13:15 -0500

    Jochen fixed the original script

--- JC_checkmotion.m
+++ JC_checkmotion.m
@@ -17,28 +17,39 @@ subfolders(~cat(1, subfolders.isdir)) = [];
 
 % iterate over subject folders
 rpfiles = {subfolders.name};
+rpfolders = rpfiles;
 for fc = 1:numel(rpfiles)
 
     % locate all rp_*.txt files
     rpfiles{fc} = dir([subfolders(fc).name '/' rppattern]);
+    rpfolders{fc} = repmat({subfolders(fc).name}, numel(rpfiles{fc}), 1);
 end
 rpfiles = cat(1, rpfiles{:});
+rpfolders = cat(1, rpfolders{:});
 
 %   iterate over all files
-%rpfolders = {rpfiles.folder};
-rpfolders = {subfolders.name};
 rpfiles = {rpfiles.name};
 rpoutliers = cell(numel(rpfiles), 1);
 for fc = 1:numel(rpfiles)
+    
+    % generate filename with path
     rpfiles{fc} = [rpfolders{fc} '/' rpfiles{fc}];
+    
+    % load RP file (into variable rp)
     rp = load(rpfiles{fc});
+    
+    % convert radian into degrees (multiplying columns 4:6 with 180/pi)
     rp(:, 4:6) = (180/pi) .* rp(:, 4:6);
+    
+    % compute frame-wise (volume-to-volume) displacement
     rpd = diff(rp);
+    
+    % find outliers (1+ as it is the first derivative) > thresholds
     rpoutliers{fc} = 1 + find(any(abs(rpd(:, 1:3)) > transthresh, 2) | ...
         any(abs(rpd(:, 4:6)) > rotthresh, 2));
 
 %     % print out information
     olist = sprintf('%d, ', rpoutliers{fc});
     olist(end-1:end) = [];
-    fprintf('%-88s: %d ([%s])\n', rpfiles{fc}, numel(rpoutliers{fc}), olist);
+    fprintf('%-32s: %d ([%s])\n', rpfiles{fc}, numel(rpoutliers{fc}), olist);
 end
