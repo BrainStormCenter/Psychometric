a7b969b314a72ca8941fbc0df1098a5225135b5f
Jason <chejux@gmail.com>
2017-10-07 17:17:32 -0500

    writes out multiple motion estimate files

--- JC_3DMC_v2.m
+++ JC_3DMC_v2.m
@@ -4,131 +4,128 @@
 %       CREATED BY:     JASON CRAGGS
 %       CREATED ON:     2017_09_29
 %
-%       USAGE:          GET INFORMATION ABOUT MOTION CORRECTION ESTIMATES
-%                       AND COMPILE THEM INTO A SINGLE FILE
+%       USAGE:          GATHER INFORMATION ABOUT MOTION CORRECTION ESTIMATES
+%                       AND WRITE OUT THE RESULTS
 
 %		MODIFIED BY:	JASON CRAGGS
 %		MODIFIED ON:	2017_10_04
-%		REASON:			NEED TO WRITE OUT RESULTS TO FILE
-
-
+%		MODIFIED ON:	2017_10_05
+%       MODIFIED ON:    2017_10_07
 %
+%%       DETAILS OF WHAT THE SCRIPT DOES
+%       SPECIFY LIBRARIES TO BE USED - IS THIS NECESSARY (?)
+%       MOTION PARAMETERS ARE STORED IN THE rp*.txt files
+%       SPECIFY THE BASE DIRECTORY OF DATA TO BE PROCESSED
+%       LOAD THE TXT FILES CONTAINING THE 3DMC ESTIMATES
+%           *   THE ESTIMATES IN THE LAST 3 COLUMNS ARE IN DEGREES
+%           *   CONVERTING RADIANS INTO DEGREES
+%       SPECIFY THE THRESHOLD FOR OUTLIER DETECTION
+%       OPEN AN OUTPUT FILE (BEFORE THE LOOP)
+%       LOOP OVER SUBJECTS
+%           *   COMPUTE FRAME-WISE DISPLACEMENT
+%              	*   A MORE SENSITIVE INDEX OF MOVEMENT
+%            	*   IDENTIFY OUTLIERS
+%           	*   WRITE EACH ITERATION TO THE OUTLIERS OUTPUT TXT FILE
+%           *   COMPUTE MAXIMUM AMOUNT OF MOTION
+%           	*   WRITE EACH ITERATION TO THE MAX MOTION OUTPUT TXT FILE
+%           *   COMPUTE MAXIMUM AMOUT OF FRAME-WISE MOTION
+%           	*   WRITE EACH ITERATION TO THE FRAMEWISE MOTION OUTLIERS OUTPUT TXT FILE
+%       *   END THE LOOP
+%       CLOST THE OUTPUT FILES
+%       STORE THE INFORMATION AS A MAT FILE
+%
+%%
 
-% libraries
-n = neuroelf;
-x = xff;
 
-% configure root path and subject pattern, as well as file patterns
-%rootpath = '/cluster/folder/craggs/study/preprocessed/';
-%rootpath = '/Volumes/Data/Imaging/R01/preprocessed/';
-%rootpath = '/Volumes/Data/Imaging/R01/preprocessed/_Jason_0/';
-rootpath = '/Volumes/Data/Imaging/R01/preprocessed/_Jason/Sub200_v1_pos/';
+%       configure root path and subject pattern, as well as file patterns
+
+rootpath = '/Volumes/Data/Imaging/R01/preprocessed/_Jason_0/';
 subpattern = 'Sub*_v*';
-%anatpattern = 'T1_*.nii';
-%funcpattern = 'RSrun*.nii';
-%numruns = 4;
+rppattern = 'rp_aRSrun*.txt';
 
-
-% find subjects in root folder
-%dirinfo = dir([rootpath subpattern]);
-%subjlist = {dirinfo.name};
-
-% pick subject according to job number
-% for sc = 1:numel(subjlist)
-% 
-%     % set primary path
-%     primary_path = [rootpath subjlist{sc} filesep];
-%     cd(primary_path);
-% end
-
-%rpfiles = n.findfiles([pwd '/Sub*'], 'rp*.txt', '-d1');
-%rpfiles = n.findfiles(['rp*.txt'], '-d1');
-
+% get subjects folders
 cd(rootpath);
+subfolders = dir(subpattern);
+subfolders(~cat(1, subfolders.isdir)) = [];
 
-rpfiles = n.findfiles(['rp*.txt']);
+%       thresholds FOR OUTLIER DETECTION
+transthresh = 1;
+rotthresh = 1;
 
-%   MOTION PARAMETERS ARE STORED IN THE rp*.txt files
-%   THE FIRST STEP IS TO LOAD THE TXT FILES
-%   BE SURE TO CONVERTING RADIANS INTO DEGREES
-%
-%rp=load('rp_aRSrun1_v1.txt');
+%       iterate over subject folders
+rpfiles = {subfolders.name};
+rpfolders = rpfiles;
+for fc = 1:numel(rpfiles)
 
-rp=load(rpfiles{1,1});
-rp2 = rp;
+    %   locate all rp_*.txt files
+    rpfiles{fc} = dir([subfolders(fc).name '/' rppattern]);
+    rpfolders{fc} = repmat({subfolders(fc).name}, numel(rpfiles{fc}), 1);
+end
+rpfiles = cat(1, rpfiles{:});
+rpfolders = cat(1, rpfolders{:});
+
+%   DECLARE ADDITIONAL VARIABLES
+rpfiles = {rpfiles.name};
+rpoutliers = cell(numel(rpfiles), 1);
+rpMax = cell(numel(rpfiles), 1);
+maxFwMot = cell(numel(rpfiles), 1);
+
+%   open an output file
+file_id1 = fopen([rootpath 'motion_outLiers.txt'], 'w');
+file_id2 = fopen([rootpath 'max_Motion.txt'], 'w');
+file_id3 = fopen([rootpath 'maxFw_Motion.txt'], 'w');
+file_id4 = fopen([rootpath 'Fw_Motion.txt'], 'w');
+
+%   iterate over all files
+for fc = 1:numel(rpfiles)
+    
+    %   generate filename with path
+    rpfiles{fc} = [rpfolders{fc} '/' rpfiles{fc}];
+    
+    %   load RP file (into variable rp)
+    rp = load(rpfiles{fc});
+    
+     %  convert radian into degrees (multiplying columns 4:6 with 180/pi)
+    rp(:, 4:6) = (180/pi) .* rp(:, 4:6);
+    
+    %   compute frame-wise (volume-to-volume) displacement
+    rpd = diff(rp);
+    
+    %   compute additional motion indicies
+    %   *   MAX MOTION & MAX FRAMEWISE
+    rpMax{fc} = (max(rp));
+    maxFwMot{fc} = (max(rpd));
+    
+    %   find outliers (1+ as it is the first derivative) > thresholds
+    rpoutliers{fc} = 1 + find(any(abs(rpd(:, 1:3)) > transthresh, 2) | ...
+        any(abs(rpd(:, 4:6)) > rotthresh, 2));
+
+    %   print out information
+    olist = sprintf('%d, ', rpoutliers{fc});
+    olist(end-1:end) = [];
+    
+    fprintf('%-32s: %d ([%s])\n', rpfiles{fc}, numel(rpoutliers{fc}), olist);
+    fprintf(file_id1,'%-32s: %d ([%s])\n', rpfiles{fc}, numel(rpoutliers{fc}), olist);
+    fprintf(file_id2,'%-32s(maxMot): %.2f %.2f %.2f %.2f %.2f %.2f \n', rpfiles{fc}, rpMax{fc});
+    fprintf(file_id3,'%-32s(maxFwMot): %.2f %.2f %.2f %.2f %.2f %.2f \n', rpfiles{fc}, maxFwMot{fc});
+        
+end
+
+fprintf(file_id4,'%.4f %.4f %.4f %.4f %.4f %.4f \n', rpd);
+
+%   close file
+fclose(file_id1);
+fclose(file_id2);
+fclose(file_id3);
+fclose(file_id4);
+
+%   TRANSPOSE THE SUBJECT LIST
+rpSubs = rpfiles';
+
+%   also store as a MAT file
+save([rootpath 'motion_indices_v1.mat'], 'rpSubs', 'rpoutliers', 'rpMax', 'maxFwMot');
 
 
-% convert radian into degrees VERSION 1
-mot = (rp * diag([1, 1, 1, 180 ./ [pi, pi, pi]]));
-% convert radian into degrees VERSION 2 (multiplying columns 4:6 with 180/pi)
-rp2(:, 4:6) = (180/pi) .* rp2(:, 4:6);
-
-
-%figure(1), plot(mot, 'DisplayName', 'mot');
-
-%   FRAME BY FRAME MOTION ESTIMATES VERSION 1
-fbf_motion = abs(diff(rp * diag([1, 1, 1, 180 ./ [pi, pi, pi]])));
-
-% compute frame-wise (volume-to-volume) displacement VERSION 2
-rpd = diff(rp2);
-
-% compute frame-wise (volume-to-volume) displacement VERSION 2
-rpd2 = abs(diff(rp2));
-
-total_motion = sum(fbf_motion);
-max_motion = max(fbf_motion);
-
-
-
-
-
-% save('text2.txt','fbf_motion', '-ascii')
-% save('text3.txt','fbf_motion')
-% save('text3.txt','fbf_motion') -ascii
-% save('text3.txt','fbf_motion', '-ascii')
-% save('text3.txt','fbf_motion', 'maxFbf', '-ascii')
-% type('text3.txt')
-% save('text3.txt', 'maxFbf', '-ascii')
-
-
-
-
-
-
-
-
-%figure(2), plot(fbf_motion * diag([1, 1, 1, 180 ./ [pi, pi, pi]]));
-%figure(2), plot(fbf_motion);
-
-% cd ../
-
-% plot(fbf_motion,'DisplayName','fbf_motion')
-% total_motion = sum(fbf_motion);
-% bar(total_motion)
-% plot(rp,'DisplayName','rp')
-% plot(rp * diag([1, 1, 1, 180 ./ [pi, pi, pi]]));
-% figure, plot(rp * diag([1, 1, 1, 180 ./ [pi, pi, pi]]));
-% bar(total_motion)
-% total_motion = sum(fbf_motion);
-% max_motion = max(fbf_motion);
-% bar(max_motion)
-% figure2, bar(max_motion)
-
-
-% set variables, number of volumes and functional slices, TR
-%nvols = 125;
-%nslices = 42;
-%TR = 2.8;
-%
-% swa = n.findfiles([pwd '/Sub*'], 'sw*.nii', '-d1');
-%
-% rpfiles = n.findfiles([pwd '/Sub*'], 'rp*.txt', '-d1');
-% %
-% % wc1 = n.findfiles([pwd '/Sub*'], 'wc1*.nii', '-d1');
-% % wc2 = n.findfiles([pwd '/Sub*'], 'wc2*.nii', '-d1');
-% % wc3 = n.findfiles([pwd '/Sub*'], 'wc3*.nii', '-d1');
-% %JC_FCprepro(swa{c}, rpfiles{c}, 120/2.8, {wc1{c}; wc2{c}; wc3{c}})
-% %
-% %
-% % THIS COMMAND WORKS FOR A SINGLE SUBJECT
-% %JC_FCprepro(swa,rpfiles,120/2.8, [wc1; wc2; wc3])
+%%
+%           END SCRIPT
+%%
