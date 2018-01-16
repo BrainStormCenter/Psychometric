2aa255d5d1908e0ce737e02341c4162229f71e08
Craggs, Jason G <craggsj@health.missouri.edu>
2018-01-16 11:39:43 -0600

    Create VOI files from .nii brain masks

--- JC_createROI.m
+++ JC_createROI.m
@@ -1,16 +1,34 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%		CREATED BY:	JASON CRAGGS
+%		CREATED ON:	2017-12-13
+%
+%		USAGE:		CONVERT AND COMBINE .NII BRAIN MASKS INTO A
+%                        SINGLE .VOI FILE
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
 
 % NeuroElf library
 n = neuroelf;
 
+maskPath = '/Volumes/Data/Imaging/R01/preprocessed/Test_files/RegionMasks/';
+
 % find ROI files
-rois = n.findfiles(FOLDER_WITH_ROI_NIFTIS, 'r???_*.nii');
+%    THESE ARE REGIONAL MASKS IN THE .nii FORMAT
+%rois = n.findfiles(FOLDER_WITH_ROI_NIFTIS, 'r???_*.nii');
+%rois = n.findfiles(maskPath, 'r???_*.nii');
+rois = n.findfiles(maskPath, 'r*.nii');
 
 % create new VOI
 voi = xff('new:voi');
 
-% import each of the files into the VOI file
+% import each of the .nii files into the VOI file
 for c=1:numel(rois)
-    nvoi.ImportClusters(rois{c}, struct('sepclus', false, 'sort', 'value'));
+    voi.ImportClusters(rois{c}, struct('sepclus', false, 'sort', 'value'));
 end
 
 % rename?
+voiName  = 'test4.voi';
+
+voi.SaveAs(voiName);
