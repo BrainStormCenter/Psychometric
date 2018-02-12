531c0d3b7fa27beeed7716901bbd91c3b707022f
Craggs, Jason G <craggsj@health.missouri.edu>
2018-02-12 15:25:48 -0600

    analyses updates

--- JC_ROIFCstats.m
+++ JC_ROIFCstats.m
@@ -9,24 +9,21 @@
 %    	MODIFIED ON:	 2017_12_13
 %   	MODIFIED ON:	  2018_02_01
 %   	MODIFIED ON:	  2018_02_02
+%       MODIFIED ON:	  2018_02_12
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%
-%
-%
-%
-%
-
+%%
+%       BASIC SETUP FOR ANALYSES
 % neuroelf library
 n = neuroelf;
 
 %         SET PRIMARY PATH
 rootpath = '/Volumes/Data/Imaging/R01/preprocessed/';
-
 cd(rootpath);
 
 % load variable
-load FCvars.mat % contains slistd!
+% contains slistd! WHICH CONATAINS THE SUBJECT LIST
+load FCvars.mat 
 
 % load VOI
 voi = xff('Craggs_VOIs.voi');
@@ -51,6 +48,7 @@ glistd = slistd(g123, :);
 rlistd = glistd(:, 4:11);
 
 % create cc arrays
+%   THESE ARE THE CROSS CORRELATIONS OF ALL THE BRAIN REGIONS LISTED IN THE VOI FILE
 afcccs = cat(3, fcccs{:});
 gfcccs = reshape(afcccs(voiorder, voiorder, rlistd(:)), [nvs, nvs, ns, 2, 2, 2]);
 
@@ -59,30 +57,40 @@ zgfcccs = n.fisherr2z(gfcccs);
 zgfcccs(isinf(zgfcccs)) = 0;
 
 % average over first and second run of each half-session
+%   THESE ARE THE 'PRE' MANIPULATION RESTING STATE SCANS
 zgfcccs = squeeze(mean(zgfcccs, 4));
 
-% this leaves dimensions
-% 1, 2 (ROIs, in order of networks, 1-16 pain, 17-22 DMN)
+% this leaves 5-dimensions
+%   THE 5 DIMENSIONS FOR THE zgfcccs ARRAY ARE:
+% 1 (ROIs IN THE PAIN NETWORK (i.e., REGIONS [1-16], AS OF February 12, 2018)
+% 2 (ROIs IN THE DMN NETWORK (i.e., REGIONS [17-22], AS OF February 12, 2018)
 % 3 subjects (in order of groups, 1-31 HC, 32-73 CLBP, 74-90 FM)
 % 4 pre/post treatment (1 pre, 2 post)
 % 5 neg/pos session (1 neg, 2 pos)
 
 % to split into within network matrices
+%   THESE ARE ARRAYS OF CROSS CORRELATIONS AMONG REGIONS IN EACH NETWORK
 painzgfcccs = zgfcccs(1:16, 1:16, :, :, :);
 dmnzgfcccs = zgfcccs(17:22, 17:22, :, :, :);
 
 % average connectivity strengths
+%   THESE ARE THE CCs FOR ALL 90 SUBJECTS ACROSS THE 16 PAIN REGIONS
 painnet = squeeze(mean(mean(painzgfcccs, 1), 2));
+%   THESE ARE THE CCs FOR ALL 90 SUBJECTS ACROSS THE 6 DMN REGIONS
 dmnnet = squeeze(mean(mean(dmnzgfcccs, 1), 2));
-
 
 % to unpack:
 % - i1 and i2 are the indices for groups HC and CLBP
 % - the next ", 1" is the "pre" (treatment) selection
 % - the next ", 1" is the "neg session" selection
+%
+%           LIST OUT THE BRAIN REGIONS IN THE PAIN AND DMN NETWORKS
+char(voinames(voiorder));
+painnames = char(voinames(voiorder(1:16)));
+dmnnames = char(voinames(voiorder(17:22)));
 
-%           T-TEST #0
-%       COMPUTING 3-GROUP ANOVA FOR THE PRE MANIPULATION RESTING STATE SCANS
+%%           ANALYSIS #0
+%       COMPUTING 3-GROUP ANOVA FOR THE PRE-MANIPULATION RESTING STATE SCANS
 %       STEP 1 = CREATE VARIABLES OF THE MEAN CORRELATION OF ALL PAIN REGIONS
 %       FOR EACH GROUP OF THE PRE SCANS ACROSS BOTH VISITS
 prePainHC = mean(painnet(i1,1,:),3);        % MEAN OF HC
@@ -91,7 +99,9 @@ prePainFM = mean(painnet(i3,1,:),3);        % MEAN OF FM
 gpNames = {'HC','CLBP','FM'};               % VARIABLE OF GROUP NAMES
 %       STEP 2 = CREATE AN ARRAY OF THE COMBINED VARIABLES FROM ABOVE
 %       THE ARRAY NEEDS TO BE PADDED BECAUSE OF UNEVEN GROUP SIZES
-A = zeros(42,3);    % INITIALIZE ARRAY OF ALL ZEROS  FOR LARGEST GROUP
+%       IDENTIFY THE LARGEST GROUP
+A = max([length(i1),length(i2),length(i3)]);
+A = zeros(A,3);    % INITIALIZE ARRAY OF ALL ZEROS  FOR LARGEST GROUP
 A(A == 0) = NaN;    % CONVERT ALL '0' TO 'NaN' (MISSING VALUES)
 A(1:length(prePainHC),1) = prePainHC;       % HC TO COLUMN 1
 A(1:length(prePainCLBP),2) = prePainCLBP;   % CLBP TO COLUMN 2
@@ -116,9 +126,7 @@ anovaOutput = [gnames(c(:,1)), gnames(c(:,2)), num2cell(c(:,3:6))];
 anovaOutput = anovaOutput(:,[1 2 6 4 3 5]);
 tableAnovaPre = array2table(anovaOutput, 'VariableNames',{'gp1','gp2', 'pval','gpDiff','lCI','uCI'});
 
-
-
-%           T-TEST #1
+%%          ANALYSIS #1 (T-TEST, HC vs. CLBP [pre, both visits])
 % to compute a compound "pre" score
 %   ttest2(group-1, pre/post, neg/post, group-2, pre/post, neg/post)
 %   Below = (HC, pre, both visits vs. CLBP, pre, both visits)
@@ -132,8 +140,7 @@ tableAnovaPre = array2table(anovaOutput, 'VariableNames',{'gp1','gp2', 'pval','g
     scanSet = (['combined pre']);
     cond = (['both']);
     Hy = ([h]);
-
-%           T-TEST #2
+%%          ANALYSIS #2 (T-TEST [HC v CLBP], COMPARING PRE FROM THE NEG VISITS, NOT OVERLY USEFUL)
 % to then run differences in group tests HC vs. CLBP
 %   ttest2(group-1, pre/post, neg/post, group-2, pre/post, neg/post)
 %   Below = (HC, pre, neg vs. CLBP, pre, neg)
@@ -148,8 +155,7 @@ tableAnovaPre = array2table(anovaOutput, 'VariableNames',{'gp1','gp2', 'pval','g
     cond = ([cond;{'neg'}]);
     Hy = ([Hy;h]);
 
-
-%           T-TEST #3
+%%           ANALYSIS #3 (T-TEST [HC v CLBP], COMPARING PRE FROM THE POS VISITS, NOT OVERLY USEFUL) 
 %   ttest2(group-1, pre/post, neg/post, group-2, pre/post, neg/post)
 %   Below = (HC, pre, pos vs. CLBP, pre, pos)
 [h, p, ci, stats] = ttest2(painnet(i1, 1, 2), painnet(i2, 1, 2), ...
@@ -161,8 +167,7 @@ tableAnovaPre = array2table(anovaOutput, 'VariableNames',{'gp1','gp2', 'pval','g
     cond = ([cond;{'pos'}]);
     Hy = ([Hy;h]);
 
-
-%           T-TEST #4
+%%           ANALYSIS #4 (T-TEST [HC v CLBP], POST > PRE (NEG), WHAT DID THE NEG MANIPULATION INCREASE)
 %   TO TEST GROUP DIFFERENCES IN POST-PRE IN THE NEGATIVE CONDITION
 %   THAT IS, GROUP DIFFERENCES IN HOW MUCH THE NEGATIVE MOOD MANIPULATION
 %   INCREASED ACTIVITY IN THE ROIs
@@ -175,8 +180,7 @@ tableAnovaPre = array2table(anovaOutput, 'VariableNames',{'gp1','gp2', 'pval','g
     cond = ([cond;{'neg'}]);
     Hy = ([Hy;h]);
 
-
-%           T-TEST #5
+%%           ANALYSIS #5 (T-TEST [HC v CLBP], POST > PRE (POS), WHAT DID THE POS MANIPULATION INCREASE)
 %   TO TEST GROUP DIFFERENCES IN POST-PRE IN THE POSITIVE CONDITION
 %   THAT IS, GROUP DIFFERENCES IN HOW MUCH THE POSITIVE MOOD MANIPULATION
 %   INCREASED ACTIVITY IN THE ROIs
@@ -189,17 +193,37 @@ tableAnovaPre = array2table(anovaOutput, 'VariableNames',{'gp1','gp2', 'pval','g
     cond = ([cond;{'pos'}]);
     Hy = ([Hy;h]);
 
+    %%           ANALYSIS #6 (T-TEST [HC v FM], POST > PRE (NEG), WHAT DID THE NEG MANIPULATION INCREASE)
+%   TO TEST GROUP DIFFERENCES IN POST-PRE IN THE NEGATIVE CONDITION
+%   THAT IS, GROUP DIFFERENCES IN HOW MUCH THE NEGATIVE MOOD MANIPULATION
+%   INCREASED ACTIVITY IN THE ROIs
+[h, p, ci, stats] = ttest2(painnet(i1, 2, 1) - painnet(i1, 1, 1), painnet(i3, 2, 1) - painnet(i3, 1, 1), ...
+    'tail', 'both', 'vartype', 'unequal');
+    Gps = ([Gps;{'HC v FM'}]);
+    pval = ([pval;p]);
+    tVal = ([tVal;stats.tstat]);
+    scanSet = ([scanSet;{'post > pre'}]);
+    cond = ([cond;{'neg'}]);
+    Hy = ([Hy;h]);
 
-char(voinames(voiorder));
-painnames = char(voinames(voiorder(1:16)));
-dmnnames = char(voinames(voiorder(17:22)));
+%%           ANALYSIS #7 (T-TEST [HC v FM], POST > PRE (POS), WHAT DID THE POS MANIPULATION INCREASE)
+%   TO TEST GROUP DIFFERENCES IN POST-PRE IN THE POSITIVE CONDITION
+%   THAT IS, GROUP DIFFERENCES IN HOW MUCH THE POSITIVE MOOD MANIPULATION
+%   INCREASED ACTIVITY IN THE ROIs
+[h, p, ci, stats] = ttest2(painnet(i1, 2, 2) - painnet(i1, 1, 2), painnet(i3, 2, 2) - painnet(i3, 1, 2), ...
+    'tail', 'both', 'vartype', 'unequal');
+    Gps = ([Gps;{'HC v FM'}]);
+    pval = ([pval;p]);
+    tVal = ([tVal;stats.tstat]);
+    scanSet = ([scanSet;{'post > pre'}]);
+    cond = ([cond;{'pos'}]);
+    Hy = ([Hy;h]);
 
-
-%       CREATING A TABLE OF OUTPUT VARIABLES
+%%       CREATING A TABLE OF OUTPUT VARIABLES
 tableTtest = table(Gps,scanSet,cond,Hy,pval,tVal,'VariableNames',{'group','scanSet','Condition','Sig','pvalue','tvalue'});
 
 
-%         SAVE WORKSPACE
+%%         SAVE WORKSPACE
 ROIFCstatsOutput = ['ROIFCstats_',datestr(now, 'dd-mm-yyyy'),'.mat']
 save(ROIFCstatsOutput);
 
